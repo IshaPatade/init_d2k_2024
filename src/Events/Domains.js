eb2d164342bfc834aa7002427cdff2eec1584deb
IshaPatade <ishaapatade14@gmail.com>
2024-01-26 04:23:15 +0530

    cv

--- src/Events/Domains.js
+++ src/Events/Domains.js
@@ -1,13 +1,13 @@
 import React from "react";
-import "./styles.css"
+import "./styles.css";
 
 const Domains = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-[5vh] ml-[-5vh]">
-      <div className="pattern pattern--blueprint flex   ">
+      <div className="pattern pattern--blueprint    ">
         <div className="domains text-white text-center  ">Computer Vision</div>
       </div>
-      <div className="pattern pattern--blueprint blue">
+      <div className="pattern pattern--blueprint ">
         <div className="domains text-white text-center  ">NLP</div>
       </div>
       <div className="pattern pattern--blueprint">
