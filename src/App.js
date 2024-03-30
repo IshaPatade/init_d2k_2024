3023394192d5fdc6b0b4cebfe162336f68cdb839
IshaPatade <ishaapatade14@gmail.com>
2024-03-30 12:12:40 +0530

    linked in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import D2k_2024 from "./Events/d2k_2024"
+import LinkedInSignIn from './LinkedIn';
 
 function App() {
   return (
     <div className="App">
-      <D2k_2024></D2k_2024>
+      {/* <D2k_2024></D2k_2024> */}
+      <LinkedInSignIn></LinkedInSignIn>
     </div>
   );
 }
